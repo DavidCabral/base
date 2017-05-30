342bbdd02fe9e2ae13996181b04b7e8a1c9cf9ac
David Cabral <davidmeloc@gmail.com>
2017-05-29 21:38:30 -0300

    add dist webapp in static folder

--- api/src/main/java/br/com/modelo/Application.java
+++ api/src/main/java/br/com/modelo/Application.java
@@ -2,16 +2,10 @@ package br.com.modelo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class Application  extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
